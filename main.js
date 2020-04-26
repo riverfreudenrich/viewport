62f1be101f88c45a2d00cf3e5e9d01073dfd3194
River Freudenrich <riverfreudenrich@gmail.com>
2020-04-26 13:34:09 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+$(window).scroll(function () {
+    $('#featured-photos .graphic').each(function () {
+        if (isScrolledIntoView(this) === true) {
+            $(this).addClass('init-animations');
+        }
+    });
+
+});
